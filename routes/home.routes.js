d6a362cb9f1bace717ef682af115e9b308f32b2a
Tamari Jacob Gray-Kaiwahia <tamarigray97@gmail.com>
2019-06-07 11:43:37 +1200

    test root redirects to weclome

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,10 +2,18 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-  res.send('Hello, World!')
+  res.redirect('/welcome')
 })
 
-router.get('/:username', (req, res) => {
+router.get('/welcome', (req, res) => {
+  //Render
+  
+  res.send('Welcome')
+})
+
+router.get('welcome/:username', (req, res) => {
+  //render
+
   res.send(`status is ${res.status}`)
 })
 
