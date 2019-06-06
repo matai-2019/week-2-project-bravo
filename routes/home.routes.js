8490064bdfbcdca3ed65a4e94ebfe389ec477659
noel <noelsoong@live.com>
2019-06-07 10:24:17 +1200

    Init Server and Middlewares

---
+++ routes/home.routes.js
@@ -0,0 +1,8 @@
+// External Imports
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('Hello, World!')
+  })
+  
+  module.exports = router
---
+++ server.js
@@ -0,0 +1,21 @@
+// External Imports
+const express = require('express')
+const hbs = require('express-handlebars')
+const server = express()
+
+// Internal Imports
+const homeRoutes = require('./routes/home.routes.js').router
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({extended: false}))
+
+// Router Middlewares
+server.use('/', homeRoutes)
+
+module.exports = server
