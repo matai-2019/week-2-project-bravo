d4c55bb22274f75b61fe81444d3352608a53d4bb
noel <noelsoong@live.com>
2019-06-07 10:50:04 +1200

    Add filesystem.js to use for Async read/write

---
+++ functions/filesystem.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+// Used for Async File Writes
+// @Params Object data, String path, Function done
+function writeFile (data, path, done) {
+  fs.writeFile(path, data, (err) => {
+    if (err) done(err)
+    done()
+  })
+}
+
+function readFile (path, type, done){
+    fs.readFile(path, type, (err, result) => {
+        if (err) done(err)
+        done(null, result)
+      })
+}
+
+// Write Binary files like images
+// @Params String name, String binary, String path, Function done
+function uploadImage (name, binary, path, done) {
+  fs.writeFile(fullPath, binary, (err) => {
+    if (err) done(err)
+    done()
+  })
+}
+
+module.exports = {
+  writeFile,
+  dirname: __dirname,
+  uploadImage
+}
